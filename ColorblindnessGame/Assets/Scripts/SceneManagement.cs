fd4c91d6e9f284e0b0417652e45d38de05e84712
AnitaLarsen <anitatamanglarsen@gmail.com>
2026-04-24 10:03:36 +0200

    Textmesh pro and start button

---
+++ ColorblindnessGame/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Ronja Scene 2");
+    }
+}
